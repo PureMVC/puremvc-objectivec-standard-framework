27482d5a9eee5c7a40ffdf022f582d7a50eb8810
Brian Knorr <brian.knorr@puremvc.org>
2008-11-26 21:35:39 +0000

    puremvc will be released as an sdk

---
+++ bin/PureMVC/iphoneos.sdk/usr/local/include/PureMVC/Proxy.h
@@ -0,0 +1,30 @@
+//
+//  Proxy.h
+//  PureMVC_ObjectiveC
+//
+//  PureMVC Port to ObjectiveC by Brian Knorr <brian.knorr@puremvc.org>
+//  PureMVC - Copyright(c) 2006-2008 Futurescale, Inc., Some rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Interfaces.h"
+#import "Notifier.h"
+
+@interface Proxy : Notifier <IProxy> {
+	id data;
+	NSString *proxyName;
+}
+
+@property(nonatomic, retain, getter=getData) id data;
+@property(nonatomic, retain, getter=getProxyName) NSString *proxyName;
+
++(id)proxy;
++(id)withProxyName:(NSString *)proxyName;
++(id)withProxyName:(NSString *)proxyName data:(id)data;
++(id)withData:(id)data;
+-(id)initWithProxyName:(NSString *)proxyName data:(id)data;
+-(void)initializeProxy;
+
++(NSString *)NAME;
+
+@end
